e21219780c5f0203bf1c167aaa486e4c4a2d4283
AbsoluteZer0-IQ <bensonmc219@outlook.com>
2022-02-04 14:50:37 -0800

    started

---
+++ capstone game/Assets/Scripts/Generation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generation : MonoBehaviour
+{
+    public int[] xpos = new int[] {1, 2, 3, 4, 5};
+    public int[] ypos = new int[] {1, 2, 3, 4, 5};
+    public int[] zpos = new int[] {1, 2, 3, 4, 5};
+    // Start is called before the first frame update
+    void Start()
+    {
+      for(int i=0; i<5; i++){
+
+      }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
