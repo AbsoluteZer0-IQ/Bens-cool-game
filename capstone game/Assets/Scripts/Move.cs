cbada8d994f093188c7f299e6a4766af096108e0
AbsoluteZer0-IQ <bensonmc219@outlook.com>
2022-02-09 10:51:28 -0800

    failed to jump

--- capstone game/Assets/Scripts/Move.cs
+++ capstone game/Assets/Scripts/Move.cs
@@ -6,33 +6,43 @@ public class Move : MonoBehaviour
 {
     public CharacterController mover;
     public int speed = 5;
-    public bool groundedPlayer;
+    public bool groundedPlayer, help;
     public Vector3 playerVelocity;
-    public float jump = 30;
-    public float gravityValue = -10;
-
+    public int jumper = 0;
+    public float garvity = -10, chngrv;
 
     void Start()
     {
       mover = GetComponent<CharacterController>();
+      help = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-      groundedPlayer = mover.isGrounded;
-      Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-      mover.Move(move * Time.deltaTime * speed);
-
-      if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-          transform.Translate(Vector3.up * Time.deltaTime);
-          //  playerVelocity.y += jump;
-        }
-    //  if(groundedPlayer == false){
-    //    playerVelocity.y += gravityValue * Time.deltaTime;
+    //  if(mover.isGrounded){
+  //      garvity = 0f;
   //    }
-    //    mover.Move(playerVelocity * Time.deltaTime);
-
+  //    else if(jumper > 0){
+//        garvity = chngrv;
+//      }
+//      else{
+//        garvity = -chngrv;
+    //  if(jumper > 0){
+    //    jumper--;
+    //  }
+    if(Input.GetAxis("Jump") == 1 && mover.isGrounded){
+      jumper = 1;
+    }
+    if(jumper > 0 && help){
+      help = false;
+      for(int i=-2; i<3; i++){
+        garvity = (-Mathf.Pow(i, 2) + 5) ;
+        jumper--;
+        Debug.Log("cum");
+      }
+    }
+      Vector3 move = new Vector3(Input.GetAxis("Horizontal"), garvity, Input.GetAxis("Vertical"));
+      mover.Move(move * Time.deltaTime * speed);
     }
 }
