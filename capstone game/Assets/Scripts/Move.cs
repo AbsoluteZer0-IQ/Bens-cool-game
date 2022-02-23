38a6a23d60def1f1de808224e15d3dcd4beeb3b8
AbsoluteZer0-IQ <64921215+AbsoluteZer0-IQ@users.noreply.github.com>
2022-02-23 11:45:36 -0800

    fix jmup matgh

--- capstone game/Assets/Scripts/Move.cs
+++ capstone game/Assets/Scripts/Move.cs
@@ -6,42 +6,46 @@ public class Move : MonoBehaviour
 {
     public CharacterController mover;
     public int speed = 5;
-    public bool groundedPlayer;
+    public bool groundedPlayer, isJump;
     public Vector3 playerVelocity;
     public int jumper = 0;
-    public float upMove = 0, upInput;
+    public float upMove = 0, upInput, garvity, sign;
 
     void Start()
     {
       mover = GetComponent<CharacterController>();
+      isJump = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-    //  if(mover.isGrounded){
-  //      garvity = 0f;
-  //    }
-  //    else if(jumper > 0){
-//        garvity = chngrv;
-//      }
-//      else{
-//        garvity = -chngrv;
-    //  if(jumper > 0){
-    //    jumper--;
-    //  }
+      if(mover.isGrounded){
+        isJump = false;
+      }
     if(Input.GetAxis("Jump") == 1 && mover.isGrounded){
       jumper = 1;
+      isJump = true;
     }
     if(jumper > 0){
       jumper = 0;
       upInput = -2;
     }
-    if(upInput <= 2){
-      upMove = (-Mathf.Pow(upInput, 2) + 5) ;
-      upInput--;
+    if(upInput < 2){
+      upMove = (-Mathf.Pow(upInput, 2) + 5) * 0.0125f;
+      upInput += 0.02f;
+      sign = -2 * upInput;
+      sign = sign/Mathf.Abs(sign);
     }
-      Vector3 move = new Vector3(Input.GetAxis("Horizontal"), upMove, Input.GetAxis("Vertical"));
+    if(!isJump){
+      Vector3 move = new Vector3(Input.GetAxis("Horizontal"), garvity, Input.GetAxis("Vertical"));
+      Vector3.Normalize(move);
       mover.Move(move * Time.deltaTime * speed);
+    }
+    else if(isJump){
+      Vector3 move = new Vector3(Input.GetAxis("Horizontal"), sign * upMove * 100, Input.GetAxis("Vertical"));
+      Vector3.Normalize(move);
+      mover.Move(move * Time.deltaTime * speed);
+    }
     }
 }
