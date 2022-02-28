ecb0326fbaefa617234e51651a51c37c8fcd227e
AbsoluteZer0-IQ <64921215+AbsoluteZer0-IQ@users.noreply.github.com>
2022-02-28 11:22:55 -0800

    finally can move
    and jump and look wow

--- capstone game/Assets/Scripts/CamCon.cs
+++ capstone game/Assets/Scripts/CamCon.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class CamCon : MonoBehaviour
 {
   public Transform spot;
+  public NewMove script;
+  public float thing;
     // Start is called before the first frame update
     void Start()
     {
-
+      script = GameObject.Find("Player").GetComponent<NewMove>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = spot.transform.position;
-        transform.rotation = spot.transform.rotation;
+//        transform.rotation = spot.transform.rotation;
+        thing = (Input.GetAxis("Mouse Y") * script.mSpeed * -1);
+        transform.Rotate(Input.GetAxis("Mouse Y") * script.mSpeed * -1, 0, 0);
+
     }
 }
--- capstone game/Assets/Scripts/NewMove.cs
+++ capstone game/Assets/Scripts/NewMove.cs
@@ -7,12 +7,25 @@ public class NewMove : MonoBehaviour
     [Range(0, 100)]
     public int speed;
     [Range(0, 100)]
+    public int jSpeed;
+    [Range(0, 100)]
     public int mSpeed;
     public float MouseX, MouseY;
+    public Vector3 look;
+    public CamCon script;
+    public bool onFloor;
     // Start is called before the first frame update
     void Start()
     {
+      onFloor = true;
+      Cursor.lockState = CursorLockMode.Locked;
+      script = GameObject.Find("Main Camera").GetComponent<CamCon>();
+    }
 
+    void OnCollisionEnter(collision other){
+      if (other.GameObject.Tag == ("Ground")){
+        onFloor = false;
+      }
     }
 
     // Update is called once per frame
@@ -21,10 +34,16 @@ public class NewMove : MonoBehaviour
         if(Input.GetAxisRaw("Vertical") == 1){
           transform.position += transform.forward * Time.deltaTime * speed;
         }
+//        MouseY = Input.GetAxis("Mouse Y") * mSpeed;
         MouseX = Input.GetAxis("Mouse X") * mSpeed;
-        if(Input.GetAxis("Mouse Y") >= 0){
-          MouseY = Input.GetAxis("Mouse Y") * mSpeed;
+        transform.Rotate(MouseY, MouseX, 0);
+        if(Input.GetAxisRaw("Jump") == 1 && !onFloor){
+          onFloor = true;
+          GetComponent<Rigidbody>().AddForce(transform.up * jSpeed);
+      //    look = transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity);
+    //      look.z = jSpeed;
+        //  look.y = 5;
+        //  GetComponent<Rigidbody>().velocity = transform.TransformDirection(look);
         }
-        transform.Rotate(MouseX, MouseY, 0);
     }
 }
