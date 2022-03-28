98879149c198a744c52f48198b4a6d087abc0e70
AbsoluteZer0-IQ <64921215+AbsoluteZer0-IQ@users.noreply.github.com>
2022-03-28 14:42:29 -0700

    continued working on noise function, experimented with random seed for each generation

--- capstone game/Assets/Scripts/Noise.cs
+++ capstone game/Assets/Scripts/Noise.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class Noise : MonoBehaviour
 {
-    [Range(0, 1)]
+    [Range(0, 100)]
     public float exe, why, zee;
     public Vector3 test;
-    public float a, b, c, cool;
+    public float a, b, c, cool, mixer;
 
+    void Start(){
+      mixer = (Random.Range(10, 101));
+    }
     void Update()
     {
       if(Input.GetKeyDown(KeyCode.E)){
         for(int i=0; i<10; i++){
           test = new Vector3(exe, why, zee);
-          a = Mathf.PerlinNoise(test.x, test.y);
-          b = Mathf.PerlinNoise(test.x, test.z);
-          c = Mathf.PerlinNoise(test.y, test.z);
+          a = Mathf.PerlinNoise(test.x/mixer, test.y/mixer);
+          b = Mathf.PerlinNoise(test.x/mixer, test.z/mixer);
+          c = Mathf.PerlinNoise(test.y/mixer, test.z/mixer);
           cool = (a+b+c)/3;
           Debug.Log(cool);
-          exe += 0.01f;
-          why += 0.01f;
-          zee += 0.01f;
+          exe += 10f;
+          why += 10f;
+          zee += 10f;
         }
       }
     }
