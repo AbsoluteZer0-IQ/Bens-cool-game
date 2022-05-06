cef961fc4da11ebdf60ce2bfb567b071f054c920
AbsoluteZer0-IQ <bensonmc219@outlook.com>
2022-05-06 12:54:45 -0700

    removed srtuypid enemy thing

--- capstone game/Assets/Scripts/Killer.cs
+++ capstone game/Assets/Scripts/Killer.cs
@@ -4,27 +4,10 @@ using UnityEngine;
 
 public class Killer : MonoBehaviour
 {
-    public Enemies script;
-
-    void Start(){
-      script = GetComponent<Enemies>();
-    }
-
     void Update()
     {
-      script = GetComponent<Enemies>();
       if(transform.position.y < -5){
         Destroy(gameObject);
       }
-      if(script.watcher == true){
-        script.watcher = false;
-        StartCoroutine(Reset());
-      }
-    }
-
-    IEnumerator Reset(){
-      yield return new WaitForSeconds(2);
-      gameObject.AddComponent<Enemies>();
-      StopCoroutine(Reset());
     }
 }
